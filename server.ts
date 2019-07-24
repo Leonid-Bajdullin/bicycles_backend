d2432185a3e7c5ef3e4808210dce008810fd2d82
Leo <lbajdull@gmail.com>
2019-07-24 22:47:53 +0300

    made a connection to DB, added endpoints

---
+++ server.ts
@@ -0,0 +1,64 @@
+// Imports
+import { createConnection } from 'typeorm';
+import { Bicycle } from './src/entity/Bicycle';
+// import {Bicycle} from "./model/Bicycle";
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+// Connections
+createConnection().then((connection) => {
+  const bicycleRepository = connection.getRepository(Bicycle);
+
+  // create and setup express app
+
+  // register routes
+
+  app.get('/get_free_bikes', async function(req, res) {
+    const freeBikes = await bicycleRepository.find({
+      where: { is_rented: false }
+    });
+    res.json(freeBikes);
+  });
+
+  app.get('/get_rented_bikes', async function(req, res) {
+    const rentedBikes = await bicycleRepository.find({
+      where: { is_rented: true }
+    });
+    return res.send(rentedBikes);
+  });
+
+  app.post('/create_bike', async function(req, res) {
+    const newBike = await bicycleRepository.create(req.body);
+    const results = await bicycleRepository.save(newBike);
+    return res.send(results);
+  });
+
+  app.put('/rent_bike/:id', async function(req, res) {
+    const bike = await bicycleRepository.findOne(req.params.id);
+    bike.is_rented = true;
+    const results = await bicycleRepository.save(bike);
+    return res.send(results);
+  });
+
+  app.put('/unrent_bike/:id', async function(req, res) {
+    const bike = await bicycleRepository.findOne(req.params.id);
+    bike.is_rented = false;
+    const results = await bicycleRepository.save(bike);
+    return res.send(results);
+  });
+
+  app.delete('/delete_bike/:id', async function(req, res) {
+    const results = await bicycleRepository.delete(req.params.id);
+    return res.send(results);
+  });
+
+  // start express server
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
